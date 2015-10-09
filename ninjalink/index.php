0251d329ecd0dc17d6372030a8b3fb8285809513
Dan-Petter Jacobsen <danpetterjacobsen@gmail.com>
2015-10-09 18:09:54 +0200

    fixed bug where disable plguin was not possible

--- ninjalink/index.php
+++ ninjalink/index.php
@@ -48,43 +48,58 @@ function ninjalink_admin_display() {
 
     $updated = false;
     $failed = false;
-
+    
     if($_POST && isset($_POST['option_page']) && $_POST['option_page'] === "ninjalink-settings" && isset($_POST['action']) && $_POST['action'] === "update"){
         $ln_id = "";
         $ln_web = "";
         $ln_blacklist = "";
 
-        if(isset($_POST['ninjalink_ln_id'])){
-            $ln_id = strtoupper(preg_replace("/[^A-Za-z0-9 ]/", '', $_POST['ninjalink_ln_id']));
+        $nulled = false;
+        if(isset($_POST['ninjalink_ln_id']) && empty(trim($_POST['ninjalink_ln_id']))){
+                update_option('ninjalink_ln_web', '');
+                $nulled = true;
         }
 
-        if(isset($_POST['ninjalink_ln_web'])){
-            $ln_web = preg_replace("/[^A-Za-z0-9 ]/", '', $_POST['ninjalink_ln_web']);
+        if(isset($_POST['ninjalink_ln_id']) && empty(trim($_POST['ninjalink_ln_web']))){
+                update_option('ninjalink_ln_web', '');
+                $nulled = true;
         }
-
-        if(isset($_POST['ninjalink_ln_blacklist'])){
-            $ln_blacklist = strtolower(str_replace(array('http://','https://','www.'),'',$_POST['ninjalink_ln_blacklist']));
-            update_option('ninjalink_ln_blacklist', $ln_blacklist);
-        }
-
-        require plugin_dir_path(__FILE__) . '/lib/api.php';
-
-        if(!empty($ln_id) && !empty($ln_web)){
-            $url = 'https://api.ninjalink.com/user/epi/validate';
-            $response = json_decode(do_post_request($url,array(
-                'Epi' => $ln_id,
-                'Website' => $ln_web
-            )));
-
-            if(isset($response->code) && $response->code == 200){
-                update_option('ninjalink_ln_web', $ln_web);
-                update_option('ninjalink_ln_id', $ln_id);
-                $updated = true;
+        
+        if(!$nulled){
+            if(isset($_POST['ninjalink_ln_id'])){
+                $ln_id = strtoupper(preg_replace("/[^A-Za-z0-9 ]/", '', $_POST['ninjalink_ln_id']));
+            }
+    
+            if(isset($_POST['ninjalink_ln_web'])){
+                $ln_web = preg_replace("/[^A-Za-z0-9 ]/", '', $_POST['ninjalink_ln_web']);
+            }
+    
+            if(isset($_POST['ninjalink_ln_blacklist'])){
+                $ln_blacklist = strtolower(str_replace(array('http://','https://','www.'),'',$_POST['ninjalink_ln_blacklist']));
+                update_option('ninjalink_ln_blacklist', $ln_blacklist);
+            }
+    
+            require plugin_dir_path(__FILE__) . '/lib/api.php';
+    
+            if(!empty($ln_id) && !empty($ln_web)){
+                $url = 'https://api.ninjalink.com/user/epi/validate';
+                $response = json_decode(do_post_request($url,array(
+                    'Epi' => $ln_id,
+                    'Website' => $ln_web
+                )));
+    
+                if(isset($response->code) && $response->code == 200){
+                    update_option('ninjalink_ln_web', $ln_web);
+                    update_option('ninjalink_ln_id', $ln_id);
+                    $updated = true;
+                } else {
+                    $failed = isset($response->message) ? 'Failed to save new settings. '.$response->message : 'Failed to save new settings. Please contact post@ninjalink.com';
+                }
             } else {
-                $failed = isset($response->message) ? 'Failed to save new settings. '.$response->message : 'Failed to save new settings. Please contact post@ninjalink.com';
+                $failed = 'Failed to save new settings. Missing "User ID" and/or "Website ID", both fields are required.';
             }
         } else {
-            $failed = 'Failed to save new settings. Missing "User ID" and/or "Website ID", both fields are required.';
+                $updated = true;
         }
     }
 
